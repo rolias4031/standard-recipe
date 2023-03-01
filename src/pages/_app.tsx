0b02fd12f7823b44700e916f8ab877d5a8c28c7c
Sailor Metz <sailormetz@Sailors-MBP.lan>
2023-02-28 23:45:00 -0600

    tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 
+import '../styles/globals.css'
+
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 import { useSession, signIn } from 'next-auth/react';
 
+
 export default function Home() {
   const [email, setEmail] = useState<string>('')
   const session = useSession();
   return (
     <div>
-      <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input  type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
       <button type="button" onClick={() => signIn("email", { email })}>signin</button>
       {email}
-      <div>{session.status === 'authenticated' ? 'Success' : 'hi'}</div>
+      <div className='border bg-gray-700'>{session.status === 'authenticated' ? 'Success' : 'hi'}</div>
     </div>
   );
 }
