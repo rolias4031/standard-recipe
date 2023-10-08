48f2192b19314502dd68d86987c56084bb96e440
Sailor Metz <67522045+rolias4031@users.noreply.github.com>
2023-10-08 14:27:09 -0500

    url fix

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,8 @@
-const LOCALHOST_BASE_URL = 'http://localhost:3000';
+const LOCALHOST_BASE_URL = 'http://localhost:3000/';
 
 function createBaseUrlFromEnvironment(env: string | undefined) {
   if (env === 'production' || env === 'preview') {
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`;
   }
   return LOCALHOST_BASE_URL;
 }
