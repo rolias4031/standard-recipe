5687d0513b7939179f1faab68380593a8e6a2f8a
Sailor Metz <67522045+rolias4031@users.noreply.github.com>
2023-10-08 23:18:12 -0500

    url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,9 @@
 const LOCALHOST_BASE_URL = 'http://localhost:3000/';
+const APP_DOMAIN_BASE_URL = 'https://standardrecipe.com/';
 
 function createBaseUrlFromEnvironment(env: string | undefined) {
   if (env === 'production' || env === 'preview') {
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`;
+    return APP_DOMAIN_BASE_URL;
   }
   return LOCALHOST_BASE_URL;
 }
